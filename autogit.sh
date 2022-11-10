8d838581ced1542e2610b0899fb301ee7672d2f0
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-11-10 14:25:21 -0300

    project started successfully

---
+++ autogit.sh
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "project started successfully"
+git branch -M main
+git remote add origin https://github.com/CarlosViniMSouza/challenge-portfolio.git
+git push -u origin main
+git status
---
+++ code/script.js
@@ -0,0 +1,1 @@
+console.log('I will see the script more later!');
